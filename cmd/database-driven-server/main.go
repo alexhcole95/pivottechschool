47bb5a3b32dd8a0da69ed1150e8b0ce203196f99
alexhcole95 <alexhcole95@gmail.com>
2022-12-31 15:21:19 -0600

    Took a different approach at main.go usage.

--- cmd/database-driven-server/main.go
+++ cmd/database-driven-server/main.go
@@ -3,265 +3,310 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"github.com/gorilla/mux"
-	_ "github.com/mattn/go-sqlite3"
-	"io/ioutil"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
+
+	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-var info []product
-
-type product struct {
-	ID          int     `json:"id"`
-	Name        string  `json:"name"`
-	Description string  `json:"description"`
-	Price       float64 `json:"price"`
+type Product struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Price       int    `json:"price"`
 }
+
+const (
+	productsPath = "./seeder/products.db"
+)
 
 var db *sql.DB
 
-// item.Name == "" || item.ID == 0 || item.Price == 0
-func (p product) IsValid() bool {
-	if p.Name == "" {
-		return false
-	}
-	if p.ID == 0 {
-		return false
-	}
-	if p.Price == 0 {
-		return false
-	}
-
-	return true
-}
-
-func initProducts() *sql.DB {
-	var err error
-	log.Println("Connecting to DB...")
-
-	db, err = sql.Open("sqlite3", "./products.db")
-	if err != nil {
-		log.Println("Could not connect to DB!")
-	}
-
-	if err = db.Ping(); err != nil {
-		log.Fatalf("Unable to reach database: %v", err)
-	}
-	log.Println("Connected to DB!")
-
-	return db
-}
-
-func getProductsHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Receiving request from client...")
-
-	lim := r.FormValue("limit")
-	l, err := strconv.Atoi(lim)
-	if err != nil {
-		http.Error(w, "Unable to convert string to int", 400)
-	}
-	sort := r.FormValue("sort")
-
-	log.Println("Request received! Searching for products...")
-	rows, err := db.Query("SELECT * FROM products ORDER BY ? LIMIT ?", sort, l)
-	if err != nil {
-		log.Println(err)
-	}
-
-	for rows.Next() {
-		var id int
-		var name string
-		var price float64
-		err = rows.Scan(&id, &name, &price)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		info = append(info, product{ID: id, Name: name, Price: price})
-	}
-
-	log.Println("Products found! Returning products to client...")
-	if err = json.NewEncoder(w).Encode(info); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
-}
-
-func getProductHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Receiving request from client...")
-
-	rows, err := db.Query("SELECT * FROM products")
+func main() {
+	database, err := sql.Open("sqlite3", productsPath)
 	if err != nil {
 		log.Fatal(err)
 	}
+	db = database
+	router := mux.NewRouter()
+	router.HandleFunc("/products", getProdsHandler).Methods(http.MethodGet)
+	router.HandleFunc("/products/{id}", getProdByIDHandler).Methods(http.MethodGet)
+	router.HandleFunc("/products", addProdHandler).Methods(http.MethodPost)
+	router.HandleFunc("/products/{id}", updateProdByIDHandler).Methods(http.MethodPut)
+	router.HandleFunc("/products/{id}", deleteProdByIDHandler).Methods(http.MethodDelete)
+	log.Println("Starting server. Listening on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
 
-	for rows.Next() {
-		var id int
-		var name string
-		var price float64
-		err = rows.Scan(&id, &name, &price)
+func getProdsHandler(w http.ResponseWriter, r *http.Request) {
+	if pingDB(w) {
+		return
+	}
+	query := "select * from products"
+	params := r.URL.Query()
+	sort := params.Get("column")
+	if sort != "" {
+		switch sort {
+		case "id", "name", "description", "price":
+			query += fmt.Sprintf(" order by %s asc", sort)
+		default:
+			log.Printf("error: %s invalid argument for column\n", sort)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+	limit := params.Get("limit")
+	if limit != "" {
+		limInt, err := strconv.Atoi(limit)
 		if err != nil {
-			log.Fatal(err)
+			log.Printf("error: %s invalid argument for limit\n", limit)
+			w.WriteHeader(http.StatusBadRequest)
+			return
 		}
-
-		info = append(info, product{ID: id, Name: name, Price: price})
+		query += fmt.Sprintf(" limit %d", limInt)
 	}
-
-	vars := mux.Vars(r)
-	itemID, err := strconv.ParseInt(vars["id"], 10, 64)
-	if err != nil {
-		http.Error(w, "Unable to convert string to int", 400)
-	}
-
-	log.Println("Request received! Searching for product...")
-	if itemID < 0 || itemID > int64(len(info)) {
-		log.Println("Product not found! Returning status to client...")
-		http.Error(w, "ID not found", 404)
-	} else {
-		for _, product := range info {
-			if int64(product.ID) == itemID {
-				log.Println("Product found! Returning product to client...")
-				if err = json.NewEncoder(w).Encode(product); err != nil {
-					w.WriteHeader(http.StatusInternalServerError)
-				}
-			}
-		}
-	}
-}
-
-func addProductHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Receiving request from client...")
-
-	var item product
-	var items []product
-	reqBody, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Unable to read request body", 400)
-	}
-	defer r.Body.Close()
-
-	if err = json.Unmarshal(reqBody, &item); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
-	items = append(info, item)
-	info = items
-
-	log.Println("Request received! Adding product to DB...")
-	if item.IsValid() == false {
-		http.Error(w, "Missing required fields", 400)
-	} else {
-		if _, err = db.Exec("INSERT INTO products (id, name, price) VALUES (?, ?, ?)", item.ID, item.Name, item.Price); err != nil {
-			log.Println(err)
-		}
-
-		log.Println("Product added to DB! Returning status to client...")
-		w.WriteHeader(http.StatusCreated)
-	}
-}
-
-func updateProductHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Receiving request from client...")
-
-	rows, err := db.Query("SELECT * FROM products")
+	rows, err := db.Query(query)
 	if err != nil {
 		log.Println(err)
-	}
-
-	for rows.Next() {
-		var id int
-		var name string
-		var price float64
-		err = rows.Scan(&id, &name, &price)
-		if err != nil {
-			log.Println(err)
-		}
-
-		info = append(info, product{ID: id, Name: name, Price: price})
-	}
-
-	vars := mux.Vars(r)
-	itemID, err := strconv.ParseInt(vars["id"], 10, 64)
-	if err != nil {
-		http.Error(w, "Unable to convert string to int", 400)
-	}
-
-	log.Println("Request received! Searching for product...")
-	reqBody, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Unable to read request body", 400)
-	}
-	defer r.Body.Close()
-
-	var updatedProduct product
-	if err = json.Unmarshal(reqBody, &updatedProduct); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
-
-	if updatedProduct.Name == "" || updatedProduct.ID == 0 || updatedProduct.Price == 0 {
-		http.Error(w, "Missing required fields", 400)
-	} else {
-		log.Println("Product found! Updating product in DB...")
-		for i, product := range info {
-			if int64(product.ID) == itemID {
-				product.ID = updatedProduct.ID
-				product.Name = updatedProduct.Name
-				product.Price = updatedProduct.Price
-				info[i] = product
-				if _, err = db.Exec("UPDATE products SET name = ?, price = ? WHERE id = ?", updatedProduct.Name, updatedProduct.Price, updatedProduct.ID); err != nil {
-					log.Println(err)
-				}
-			}
+	defer rows.Close()
+	var prods []Product
+	for rows.Next() {
+		var prod Product
+		if err = rows.Scan(&prod.ID, &prod.Name, &prod.Description, &prod.Price); err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
+		prods = append(prods, prod)
 	}
+	if err := rows.Err(); err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	bs, err := json.Marshal(prods)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Add("content-type", "application/json")
+	if _, err := w.Write(bs); err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
 
-	log.Println("Product updated in DB! Returning status to client...")
+func getProdByIDHandler(w http.ResponseWriter, r *http.Request) {
+	if pingDB(w) {
+		return
+	}
+	vars := mux.Vars(r)
+	id := vars["id"]
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	query := fmt.Sprintf("select * from products where id=%d", idInt)
+	var prod Product
+	if err := db.QueryRow(query).Scan(&prod.ID, &prod.Name, &prod.Description, &prod.Price); err != nil {
+		if err == sql.ErrNoRows {
+			w.WriteHeader(http.StatusNotFound)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		return
+	}
+	bs, err := json.Marshal(prod)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Add("content-type", "application/json")
+	if _, err := w.Write(bs); err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func addProdHandler(w http.ResponseWriter, r *http.Request) {
+	if pingDB(w) {
+		return
+	}
+	body, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	var prod Product
+	if err := json.Unmarshal(body, &prod); err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if prod.ID != 0 || len(strings.Fields(prod.Name)) == 0 || len(strings.Fields(prod.Description)) == 0 || prod.Price == 0 {
+		log.Println("error: invalid json payload from post request")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	query := "insert into products(name, description, price) values(?, ?, ?)"
+	tx, err := db.Begin()
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	stmt, err := tx.Prepare(query)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if _, err := stmt.Exec(prod.Name, prod.Description, prod.Price); err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if err := tx.Commit(); err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 	w.WriteHeader(http.StatusCreated)
 }
 
-func deleteProductHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Receiving request from client...")
-	var newProduct []product
-
-	vars := mux.Vars(r)
-	itemID, err := strconv.ParseInt(vars["id"], 10, 64)
+func updateProdByIDHandler(w http.ResponseWriter, r *http.Request) {
+	if pingDB(w) {
+		return
+	}
+	id := mux.Vars(r)["id"]
+	idInt, err := strconv.Atoi(id)
 	if err != nil {
-		http.Error(w, "Unable to convert string to int", 400)
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
-
-	if itemID < 0 || itemID > int64(len(info)) {
-		log.Println("Product not found! Returning status to client...")
-		http.Error(w, "ID not found", 404)
-	} else {
-		log.Println("Request received! Searching for product...")
-		for _, product := range info {
-			if int64(product.ID) != itemID {
-				newProduct = append(newProduct, product)
-			}
-		}
-		info = newProduct
-
-		log.Println("Product found! Deleting product from DB...")
-		if _, err = db.Exec("DELETE FROM products WHERE id = ?", itemID); err != nil {
-			log.Println(err)
-		}
-
-		log.Println("Product deleted from DB! Returning status to client...")
-		w.WriteHeader(http.StatusOK)
+	body, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
+	var oldProd Product
+	if err := db.QueryRow("select * from products where id=?", idInt).Scan(&oldProd.ID, &oldProd.Name, &oldProd.Description, &oldProd.Price); err != nil {
+		log.Println(err)
+		if err == sql.ErrNoRows {
+			w.WriteHeader(http.StatusNotFound)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		return
+	}
+	var newProd Product
+	if err := json.Unmarshal(body, &newProd); err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if (idInt != newProd.ID && newProd.ID != 0) || len(strings.Fields(newProd.Name)) == 0 || len(strings.Fields(newProd.Description)) == 0 || newProd.Price == 0 {
+		log.Println("error: invalid json payload from put request")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	query := "update products set name = ?, description = ?, price = ? where id = ?"
+	tx, err := db.Begin()
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	stmt, err := tx.Prepare(query)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if _, err := stmt.Exec(newProd.Name, newProd.Description, newProd.Price, idInt); err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if err := tx.Commit(); err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 }
 
-func main() {
-	log.Println("Starting server...")
-	initProducts()
+func deleteProdByIDHandler(w http.ResponseWriter, r *http.Request) {
+	if pingDB(w) {
+		return
+	}
+	id := mux.Vars(r)["id"]
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	var prod Product
+	if err := db.QueryRow("select * from products where id=?", idInt).Scan(&prod.ID, &prod.Name, &prod.Description, &prod.Price); err != nil {
+		log.Println(err)
+		if err == sql.ErrNoRows {
+			w.WriteHeader(http.StatusNotFound)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		return
+	}
+	query := "delete from products where id=?"
+	tx, err := db.Begin()
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	stmt, err := tx.Prepare(query)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if _, err := stmt.Exec(idInt); err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if err := tx.Commit(); err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
 
-	r := mux.NewRouter()
-	r.HandleFunc("/products", getProductsHandler).Methods("GET")
-	r.HandleFunc("/products/{id}", getProductHandler).Methods("GET")
-	r.HandleFunc("/products", addProductHandler).Methods("POST")
-	r.HandleFunc("/products/{id}", updateProductHandler).Methods("PUT")
-	r.HandleFunc("/products/{id}", deleteProductHandler).Methods("DELETE")
-
-	log.Println("Listening on Port 8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+func pingDB(w http.ResponseWriter) bool {
+	if err := db.Ping(); err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return true
+	}
+	return false
 }
